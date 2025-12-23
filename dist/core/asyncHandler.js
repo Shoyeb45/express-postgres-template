53aa336e0db3778f4ce3ce081e3e3ba4f0559de0
Shoyeb45 <shoyebff45@gmail.com>
2025-12-23 10:46:28 +0530

    Remove dist from tracking and ignore it

--- dist/core/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-function asyncHandler(execution) {
-    return (req, res, next) => {
-        execution(req, res, next).catch(next);
-    };
-}
-//# sourceMappingURL=asyncHandler.js.map
--- dist/types/Role.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleCode = void 0;
-var RoleCode;
-(function (RoleCode) {
-    RoleCode["USER"] = "user";
-    RoleCode["ADMIN"] = "admin";
-})(RoleCode || (exports.RoleCode = RoleCode = {}));
-//# sourceMappingURL=Role.js.map
